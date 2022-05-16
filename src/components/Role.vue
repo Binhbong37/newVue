47e60c4c1eb351020208f81a827ecd26f2f2c660
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-05-16 11:27:18 +0700

    Edit file role

--- src/components/Role.vue
+++ src/components/Role.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>ROLE</div>
+  <div>
+    <h1>PAGE ROLE</h1>
+    <button class="bg-emerald-500 p-2 rounded">
+      <router-link to="/">Back to home</router-link>
+    </button>
+  </div>
 </template>
 
 <script>
