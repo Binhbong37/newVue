7297d7c8af7450a864365ae9006378eeaeb0a159
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-05-16 12:06:51 +0700

    Add des in file role

--- src/components/Role.vue
+++ src/components/Role.vue
@@ -3,6 +3,7 @@
     <h1>PAGE ROLE</h1>
 
     <p>Change role here</p>
+    <p>Test role file</p>
 
     <button class="bg-emerald-500 p-2 rounded">
       <router-link to="/">Back to home</router-link>
