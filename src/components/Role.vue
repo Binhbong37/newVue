98c67d09854cfa2a7436a99d3c7fb782edee8491
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-05-16 14:27:58 +0700

    New branch to test

--- src/components/Role.vue
+++ src/components/Role.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <h1>PAGE ROLE</h1>
-
-    <p>Change role here</p>
-    <p>Test role file</p>
+    <h1 class="text-2xl font-bold">PAGE ROLE</h1>
+    <p>Create PUll request</p>
 
     <button class="bg-emerald-500 p-2 rounded">
       <router-link to="/">Back to home</router-link>
